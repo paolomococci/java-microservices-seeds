7bb420560f26b7c7b8c9e970740da2a847c28a9e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-10-09 22:26:45 +0200

    Update SeedRepository.java

--- pm-jms/src/main/java/local/example/seed/repository/SeedRepository.java
+++ pm-jms/src/main/java/local/example/seed/repository/SeedRepository.java
@@ -20,7 +20,11 @@ package local.example.seed.repository;
 
 import local.example.seed.model.Seed;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@RepositoryRestResource
+@CrossOrigin(origins = "http://127.0.0.1")
 public interface SeedRepository 
         extends CrudRepository<Seed, Long> {
     
