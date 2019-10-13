4547cec6d9e0ad19aa7c145f0b172481d708b97b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-10-13 04:45:01 +0200

    Create SeedNotFoundException.java

---
+++ pm-jms-provider/src/main/java/local/example/seed/exception/SeedNotFoundException.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.exception;
+
+public class SeedNotFoundException 
+        extends RuntimeException {
+
+    public SeedNotFoundException(long id) {
+        super("could not find seed with id: " + id);
+    }
+}
