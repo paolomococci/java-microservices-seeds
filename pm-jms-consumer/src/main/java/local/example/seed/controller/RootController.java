d04556da4da4820b4eef53ff2c427229300e6c5d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-10-14 19:42:22 +0200

    Update RootController.java

--- pm-jms-consumer/src/main/java/local/example/seed/controller/RootController.java
+++ pm-jms-consumer/src/main/java/local/example/seed/controller/RootController.java
@@ -18,6 +18,12 @@
 
 package local.example.seed.controller;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +38,39 @@ public class RootController {
                     name="name", 
                     required=false, 
                     defaultValue = "seeds"
-            ) String name, Model model) {
+            ) String name, Model model) 
+            throws ProtocolException,
+            IOException {
         model.addAttribute("name", name);
+        model.addAttribute("result", this.retriever());
         return "root";
+    }
+    
+    private String retriever() 
+            throws MalformedURLException, 
+            ProtocolException, 
+            IOException {
+        String result = "";
+        try {
+            URL url = new URL("http://127.0.0.1:9090/api/seeds");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if (responseCode == 200) {
+                try (Scanner scanner = new Scanner(url.openStream())) {
+                    while (scanner.hasNext()) {
+                        result+= scanner.next();
+                    }
+                    scanner.close();
+                }
+            } else {
+                throw new RuntimeException("http response code: " + responseCode);
+            }
+            connection.disconnect();
+        } catch (IOException | RuntimeException e) {
+            e.getMessage();
+        }
+        return result;
     }
 }
