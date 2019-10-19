86becf51a53f1ea262e25fdeeed1bb0f70113605
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-10-19 18:11:05 +0200

    Update Seeds.java

--- pm-jms-consumer/src/main/java/local/example/seed/model/Seeds.java
+++ pm-jms-consumer/src/main/java/local/example/seed/model/Seeds.java
@@ -19,14 +19,133 @@
 package local.example.seed.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Seeds {
+public class Seeds implements List<Seed> {
+    
     private List<Seed> seeds = new ArrayList<>();
+
+    @Override
+    public int size() {
+        return seeds.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return seeds.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        return seeds.contains(object);
+    }
+
+    @Override
+    public Iterator<Seed> iterator() {
+        return seeds.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return seeds.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] ts) 
+            throws ArrayStoreException, 
+            NullPointerException {
+        return seeds.toArray(ts);
+    }
+
+    @Override
+    public boolean add(Seed seed) {
+        return seeds.add(seed);
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        return seeds.remove(object);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return seeds.containsAll(collection);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Seed> collection) {
+        return seeds.addAll(collection);
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends Seed> collection) {
+        return seeds.addAll(i, collection);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return seeds.removeAll(collection);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return seeds.retainAll(collection);
+    }
+
+    @Override
+    public void clear() {
+        seeds.clear();
+    }
+
+    @Override
+    public Seed get(int i) {
+        return seeds.get(i);
+    }
+
+    @Override
+    public Seed set(int i, Seed seed) {
+        return seeds.set(i, seed);
+    }
+
+    @Override
+    public void add(int i, Seed seed) {
+        seeds.add(i, seed);
+    }
+
+    @Override
+    public Seed remove(int i) {
+        return seeds.remove(i);
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        return seeds.indexOf(object);
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        return seeds.lastIndexOf(object);
+    }
+
+    @Override
+    public ListIterator<Seed> listIterator() {
+        return seeds.listIterator();
+    }
+
+    @Override
+    public ListIterator<Seed> listIterator(int i) {
+        return seeds.listIterator(i);
+    }
+
+    @Override
+    public List<Seed> subList(int i, int j) {
+        return seeds.subList(i, j);
+    }
 }
