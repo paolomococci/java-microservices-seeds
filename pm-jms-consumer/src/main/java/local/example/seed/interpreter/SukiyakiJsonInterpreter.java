c476f9e95e03656b532e1ca5eea238e9904eb3b4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-10-20 17:22:12 +0200

    Update SukiyakiJsonInterpreter.java

--- pm-jms-consumer/src/main/java/local/example/seed/interpreter/SukiyakiJsonInterpreter.java
+++ pm-jms-consumer/src/main/java/local/example/seed/interpreter/SukiyakiJsonInterpreter.java
@@ -19,6 +19,7 @@
 package local.example.seed.interpreter;
 
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
 import com.jayway.jsonpath.ReadContext;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -31,46 +32,27 @@ import local.example.seed.model.Seeds;
  */
 public class SukiyakiJsonInterpreter {
     
-    public List<String> deserialize(String dataString) {
-        ReadContext readContext = JsonPath.parse(dataString);
-        return readContext.read("$._embedded.seeds[*]");
-    }
-    
-    public static String toStrings(String dataString) {
-        Seeds seeds = new Seeds();
-        String jsonDataString = dataString;
-        String jsonPathExpression = "$._embedded.seeds[*]";
-        JsonPath jsonPath = JsonPath.compile(jsonPathExpression);
-        List<Seed> seedList = jsonPath.read(jsonDataString);
-        int sizeOfSeedList = seedList.size();
-        for (int i = 0; i < sizeOfSeedList; i++) {
-            Seed s = new Seed();
-            ReadContext rc = JsonPath.parse(jsonDataString);
-            s.setId(Long.parseLong(rc.read("$._embedded.seeds[" + i + "].id").toString()));
-            s.setName(rc.read("$._embedded.seeds[" + i + "].name"));
-            s.setPercentage(Double.parseDouble(rc.read("$._embedded.seeds[" + i + "].percentage").toString()));
-            s.setCreated(LocalDateTime.parse(rc.read("$._embedded.seeds[" + i + "].created").toString()));
-            seeds.add(s);
+    public static Seeds deserialize(String dataString) {
+        try {
+            Seeds seeds = new Seeds();
+            String jsonDataString = dataString;
+            String jsonPathExpression = "$._embedded.seeds[*]";
+            JsonPath jsonPath = JsonPath.compile(jsonPathExpression);
+            List<Seed> seedList = jsonPath.read(jsonDataString);
+            int sizeOfSeedList = seedList.size();
+            for (int i = 0; i < sizeOfSeedList; i++) {
+                Seed s = new Seed();
+                ReadContext rc = JsonPath.parse(jsonDataString);
+                s.setId(Long.parseLong(rc.read("$._embedded.seeds[" + i + "].id").toString()));
+                s.setName(rc.read("$._embedded.seeds[" + i + "].name"));
+                s.setPercentage(Double.parseDouble(rc.read("$._embedded.seeds[" + i + "].percentage").toString()));
+                s.setCreated(LocalDateTime.parse(rc.read("$._embedded.seeds[" + i + "].created").toString()));
+                seeds.add(s);
+            }
+            return seeds;
+        } catch (PathNotFoundException e) {
+            System.err.println(e.getMessage());
+            return null;
         }
-        return seeds.toString();
-    }
-    
-    public static Seeds toSeeds(String dataString) {
-        Seeds seeds = new Seeds();
-        String jsonDataString = dataString;
-        String jsonPathExpression = "$._embedded.seeds[*]";
-        JsonPath jsonPath = JsonPath.compile(jsonPathExpression);
-        List<Seed> seedList = jsonPath.read(jsonDataString);
-        int sizeOfSeedList = seedList.size();
-        for (int i = 0; i < sizeOfSeedList; i++) {
-            Seed s = new Seed();
-            ReadContext rc = JsonPath.parse(jsonDataString);
-            s.setId(Long.parseLong(rc.read("$._embedded.seeds[" + i + "].id").toString()));
-            s.setName(rc.read("$._embedded.seeds[" + i + "].name"));
-            s.setPercentage(Double.parseDouble(rc.read("$._embedded.seeds[" + i + "].percentage").toString()));
-            s.setCreated(LocalDateTime.parse(rc.read("$._embedded.seeds[" + i + "].created").toString()));
-            seeds.add(s);
-        }
-        return seeds;
     }
 }
