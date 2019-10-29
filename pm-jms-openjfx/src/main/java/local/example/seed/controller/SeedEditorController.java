5cbc428d2c1ab9dac5e587bace43f6658298712c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-10-29 10:17:17 +0100

    Update SeedEditorController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/SeedEditorController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/SeedEditorController.java
@@ -18,6 +18,8 @@
 
 package local.example.seed.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -25,58 +27,92 @@ import local.example.seed.model.Seed;
 import local.example.seed.view.InvalidFormAlertView;
 
 public class SeedEditorController {
-    
+
+    private final Pattern idPattern;
+    private final Pattern namePattern;
+    private final Pattern percentagePattern;
+
     private Stage editorStage;
     private Seed editedSeed;
     private boolean onClicked = false;
     private String eventualityErrorMessage = "";
-    
+
     @FXML
     private TextField idField;
-    
+
     @FXML
     private TextField nameField;
-    
+
     @FXML
     private TextField percentageField;
-    
+
     @FXML
     private TextField createdField;
-    
+
+    public SeedEditorController() {
+        this.idPattern = Pattern.compile("[\\d]+");
+        this.namePattern = Pattern.compile("[\\w]");
+        this.percentagePattern = Pattern.compile("0.[\\d]");
+    }
+
     @FXML
     private void initialize() {}
-    
+
     private boolean isValidForm() {
-        return this.isValidId() && 
-                this.isValidName() && 
-                this.isValidPercentage();
+        return this.isValidId()
+                && this.isValidName()
+                && this.isValidPercentage();
     }
-    
-    private boolean isValidId() { // TODO use regular expression
-        String temp = this.idField.getText();
-        if (temp != null && !"".equals(temp)) {
-            return true;
+
+    private boolean isValidId() {
+        try {
+            String temp = this.idField.getText();
+            Matcher matcher = this.idPattern.matcher(temp);
+            if (matcher.find() && matcher.group().equals(temp)) {
+                return true;
+            }
+            this.eventualityErrorMessage += "id field have not valid entry\n";
+            return false;
+        } catch (RuntimeException e) {
+            this.eventualityErrorMessage += "id field have mistaken value, runtime error\n";
+            return false;
+        } catch (Exception e) {
+            this.eventualityErrorMessage += "id field have mistaken value\n";
+            return false;
         }
-        this.eventualityErrorMessage+= "id field is empty\n";
-        return false;
     }
-    
-    private boolean isValidName() { // TODO use regular expression
-        String temp = this.nameField.getText();
-        if (temp != null && !"".equals(temp)) {
-            return true;
+
+    private boolean isValidName() {
+        try {
+            String temp = this.nameField.getText();
+            Matcher matcher = this.namePattern.matcher(temp);
+            if (matcher.find()) {
+                return true;
+            }
+            this.eventualityErrorMessage += "name field have not valid entry\n";
+            return false;
+        } catch (Exception e) {
+            this.eventualityErrorMessage += "name field have mistaken entry\n";
+            return false;
         }
-        this.eventualityErrorMessage+= "name field is empty\n";
-        return false;
     }
-    
-    private boolean isValidPercentage() { // TODO use regular expression
-        String temp = this.percentageField.getText();
-        if (temp != null && !"".equals(temp)) {
-            return true;
+
+    private boolean isValidPercentage() {
+        try {
+            String temp = this.percentageField.getText();
+            Matcher matcher = this.percentagePattern.matcher(temp);
+            if (matcher.find()) {
+                return true;
+            }
+            this.eventualityErrorMessage += "percentage field have not valid entry\n";
+            return false;
+        } catch (RuntimeException e) {
+            this.eventualityErrorMessage += "percentage field have mistaken value, runtime error\n";
+            return false;
+        } catch (Exception e) {
+            this.eventualityErrorMessage += "percentage field have mistaken value\n";
+            return false;
         }
-        this.eventualityErrorMessage+= "percentage field is empty\n";
-        return false;
     }
 
     @FXML
@@ -91,24 +127,26 @@ public class SeedEditorController {
             InvalidFormAlertView.showAlert(this.editorStage, this.eventualityErrorMessage);
         }
     }
-    
+
     @FXML
     public void cancel() {
         this.editorStage.close();
     }
-    
+
     public void setEditorStage(Stage stage) {
         this.editorStage = stage;
     }
-    
+
     public void setSeed(Seed seed) {
         this.editedSeed = seed;
         this.idField.setText(seed.getIdValue());
+        this.idField.setPromptText("only integer digits");
         this.nameField.setText(seed.getNameValue());
+        this.nameField.setPromptText("only alphabetical characters");
         this.percentageField.setText(seed.getPercentageValue());
-        this.percentageField.setPromptText("0 <= n <= 1");
+        this.percentageField.setPromptText("0 < n < 1");
     }
-    
+
     public boolean isOnClicked() {
         return this.onClicked;
     }
