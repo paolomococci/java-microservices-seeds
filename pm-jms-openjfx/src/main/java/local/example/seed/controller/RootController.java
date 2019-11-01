00d81cb136c4d3baaf0069b7f9f613594bddeb27
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-01 16:46:29 +0100

    Update RootController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/RootController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/RootController.java
@@ -36,6 +36,11 @@ public class RootController {
     }
     
     @FXML
+    private void menuItemPieChart() {
+        
+    }
+    
+    @FXML
     private void menuItemAbout() {
         MenuItemAboutView.showAlert();
     }
