cc65ef3f276e081afa10a00c214de05d384a2636
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-03 17:18:27 +0100

    Update LineChartViewController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/LineChartViewController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/LineChartViewController.java
@@ -18,9 +18,38 @@
 
 package local.example.seed.controller;
 
-public class LineChartViewController {
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 
-    public void getLineChart() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }    
+public class LineChartViewController {
+    
+    @FXML
+    private LineChart<String, Number> sagrantinoLineChart;
+    
+    private XYChart.Series<String, Number> sagrantinoDataSeries;
+    
+    @FXML
+    private void initialize() {}
+    
+    @FXML
+    public void loadSagrantinoData() {
+        sagrantinoLineChart.getData().clear();
+        sagrantinoDataSeries = new XYChart.Series<>();
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1960", 8));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1965", 12));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1970", 18));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1975", 25));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1980", 30));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1985", 55));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1990", 95));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("1995", 130));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("2000", 155));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("2005", 190));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("2010", 205));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("2015", 210));
+        sagrantinoDataSeries.getData().add(new XYChart.Data("2019", 215));
+        sagrantinoDataSeries.setName("fantasy seasonal harvests");
+        sagrantinoLineChart.getData().add(sagrantinoDataSeries);
+    }
 }
