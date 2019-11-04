16cfa298d0e063a9a06fb3275e543143bc42adee
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-04 16:12:21 +0100

    Update BarChartViewController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/BarChartViewController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/BarChartViewController.java
@@ -18,7 +18,70 @@
 
 package local.example.seed.controller;
 
-public class BarChartViewController {
+import java.util.Random;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 
+public class BarChartViewController {
     
+    @FXML
+    private BarChart<String, Double> pseudoRandomBarChart;
+    
+    private ObservableList<XYChart.Series<String, Double>> observableList;
+    
+    private final Random random = new Random();
+
+    @FXML
+    private void initialize() {}
+    
+    @FXML
+    public void loadPseudoRandomData() {
+        pseudoRandomBarChart.getData().clear();
+        observableList = FXCollections.observableArrayList();
+        Series<String, Double> sagrantinoSeries = new Series<>();
+        Series<String, Double> sangioveseSeries = new Series<>();
+        Series<String, Double> colorinoSeries = new Series<>();
+        Series<String, Double> cigliegioloSeries = new Series<>();
+        Series<String, Double> canaioloSeries = new Series<>();
+        sagrantinoSeries.setName("sagrantino");
+        sangioveseSeries.setName("sangiovese");
+        colorinoSeries.setName("colorino");
+        cigliegioloSeries.setName("cigliegiolo");
+        canaioloSeries.setName("canaiolo");
+        for (int i = 1960; i < 2020; i += 5) {
+            sagrantinoSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            sangioveseSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            colorinoSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            cigliegioloSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            canaioloSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+        }
+        observableList.addAll(
+                sagrantinoSeries, 
+                sangioveseSeries, 
+                colorinoSeries, 
+                cigliegioloSeries, 
+                canaioloSeries
+        );
+        pseudoRandomBarChart.setTitle("pseudo-random wine values");
+        pseudoRandomBarChart.getData().addAll(observableList);
+    }
 }
