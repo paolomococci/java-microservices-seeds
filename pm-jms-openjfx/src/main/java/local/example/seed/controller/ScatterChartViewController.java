0e9d25d25120365e60fde197d26e60b124c246f7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-05 08:10:44 +0100

    Update ScatterChartViewController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/ScatterChartViewController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/ScatterChartViewController.java
@@ -18,7 +18,72 @@
 
 package local.example.seed.controller;
 
-public class ScatterChartViewController {
+import java.util.Random;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 
+public class ScatterChartViewController {
     
+    @FXML
+    private ScatterChart<String, Double> pseudoRandomScatterChart;
+    
+    private final Random random = new Random();
+    
+    @FXML
+    private void initialize() {}
+
+    @FXML
+    public void loadPseudoRandomData() {
+        pseudoRandomScatterChart.getData().clear();
+        pseudoRandomScatterChart.setTitle("pseudo-random wine values");
+        pseudoRandomScatterChart.setData(makeSeries());
+    }
+    
+    private ObservableList<XYChart.Series<String, Double>> makeSeries() {
+        ObservableList<XYChart.Series<String, Double>> observableListSeries;
+        observableListSeries = FXCollections.observableArrayList();
+        XYChart.Series<String, Double> sagrantinoSeries = new XYChart.Series<>();
+        XYChart.Series<String, Double> sangioveseSeries = new XYChart.Series<>();
+        XYChart.Series<String, Double> colorinoSeries = new XYChart.Series<>();
+        XYChart.Series<String, Double> cigliegioloSeries = new XYChart.Series<>();
+        XYChart.Series<String, Double> canaioloSeries = new XYChart.Series<>();
+        sagrantinoSeries.setName("sagrantino");
+        sangioveseSeries.setName("sangiovese");
+        colorinoSeries.setName("colorino");
+        cigliegioloSeries.setName("cigliegiolo");
+        canaioloSeries.setName("canaiolo");
+        for (int i = 1960; i < 2020; i += 5) {
+            sagrantinoSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            sangioveseSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            colorinoSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            cigliegioloSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+            canaioloSeries.getData().add(
+                            new XYChart.Data<>(
+                                    Integer.toString(i), random.nextDouble()
+                            ));
+        }
+        observableListSeries.addAll(
+                sagrantinoSeries, 
+                sangioveseSeries, 
+                colorinoSeries, 
+                cigliegioloSeries, 
+                canaioloSeries
+        );
+        return observableListSeries;
+    }
 }
