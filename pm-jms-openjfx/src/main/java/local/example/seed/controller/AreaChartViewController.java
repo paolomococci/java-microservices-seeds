bd7eebbcf1a883d89fab00df770b35a0f1c35c2b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-06 11:46:11 +0100

    Update AreaChartViewController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/AreaChartViewController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/AreaChartViewController.java
@@ -19,14 +19,24 @@
 package local.example.seed.controller;
 
 import java.util.Random;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 public class AreaChartViewController {
     
     @FXML
     private AreaChart<String, Number> pseudoRandomAreaChart;
+    
+    @FXML
+    private CategoryAxis abscissa;
+    
+    @FXML
+    private NumberAxis ordinate;
     
     private XYChart.Series<String, Number> pseudoRandomDataSeries;
     
@@ -38,14 +48,37 @@ public class AreaChartViewController {
     @FXML
     public void loadPseudoRandomData() {
         pseudoRandomAreaChart.getData().clear();
+        ordinate.setAutoRanging(false);
+        ordinate.setLowerBound(0.0);
+        ordinate.setUpperBound(110.0);
+        ordinate.setLabel("quantity");
+        abscissa.setAutoRanging(false);
+        abscissa.setLabel("seasonal harvests");
+        abscissa.setTickLabelRotation(90.0);
+        abscissa.setCategories(this.makeStringObservableList());
         pseudoRandomDataSeries = new XYChart.Series<>();
-        pseudoRandomDataSeries.setName("pseudo-random series");
+        pseudoRandomDataSeries.setName("sagrantino wine fantasy data");
         for (int i = 1945; i < 2020; i++) {
             pseudoRandomDataSeries.getData().add(
-                    new XYChart.Data<>(Integer.toString(i), random.nextDouble())
+                    new XYChart.Data<>(
+                            Integer.toString(i), 
+                            random.nextDouble()*100.0
+                    )
             );
         }
-        pseudoRandomAreaChart.setTitle("1945-2019 pseudo random data");
-        pseudoRandomAreaChart.getData().add(pseudoRandomDataSeries);
+        pseudoRandomAreaChart
+                .setTitle("1945-2019 pseudo random data series of sagrantino wine");
+        pseudoRandomAreaChart
+                .getData()
+                .add(pseudoRandomDataSeries);
+    }
+    
+    private ObservableList<String> makeStringObservableList() {
+        ObservableList<String> temp;
+        temp = FXCollections.observableArrayList();
+        for (int i = 1940; i < 2025; i+=5) {
+            temp.add(Integer.toString(i));
+        }
+        return temp;
     }
 }
