86fff0b9f444c28fd2ce5f855c6e0291ffd9666a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-10 15:29:52 +0100

    Update SampleThreeViewController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/SampleThreeViewController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/SampleThreeViewController.java
@@ -18,6 +18,57 @@
 
 package local.example.seed.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.Camera;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.SubScene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import local.example.seed.view.util.WhiteScene;
+
 public class SampleThreeViewController {
     
+    @FXML
+    private AnchorPane mainAnchorPane;
+    
+    private Camera camera;
+    private PhongMaterial phongMaterial;
+    private Box box;
+    private SubScene subScene;
+    
+    @FXML
+    private void initialize() {
+        camera = new PerspectiveCamera(false);
+        camera.getTransforms().addAll(
+                new Rotate(-20.0, Rotate.Y_AXIS),
+                new Rotate(-20.0, Rotate.X_AXIS),
+                new Translate(0.0, 0.0, -15.0)
+        );
+        phongMaterial = new PhongMaterial();
+        phongMaterial.setSpecularColor(Color.CORAL);
+        phongMaterial.setDiffuseColor(Color.RED);
+        box = new Box(150.0, 150.0, 150.0);
+        box.setMaterial(phongMaterial);
+        box.setDrawMode(DrawMode.FILL);
+        box.setTranslateX(300.0);
+        box.setTranslateY(200.0);
+        box.setTranslateZ(75.0);
+        box.setRotate(0.0);
+        subScene = WhiteScene.createWhiteScene(
+                box, 
+                mainAnchorPane.getPrefWidth(),
+                mainAnchorPane.getPrefHeight(),
+                Color.ALICEBLUE, 
+                camera, 
+                true
+        );
+        mainAnchorPane.getChildren().add(subScene);
+        mainAnchorPane.getChildren().add(new Label("sample cube"));
+    }
 }
