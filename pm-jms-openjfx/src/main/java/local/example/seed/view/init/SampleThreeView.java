fb10359a78cd9bc385bdb38ee2d51096ddb7b89f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-10 18:27:57 +0100

    Update SampleThreeView.java

--- pm-jms-openjfx/src/main/java/local/example/seed/view/init/SampleThreeView.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/view/init/SampleThreeView.java
@@ -18,6 +18,39 @@
 
 package local.example.seed.view.init;
 
+import java.io.IOException;
+import java.util.Arrays;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import local.example.seed.App;
+import local.example.seed.view.alert.ExceptionAlertView;
+
 public class SampleThreeView {
     
+    public static void init(App app) {
+        var uri = "view/sample-three-view.fxml";
+        var title = "sample three 3D";
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(App.class.getResource(uri));
+        AnchorPane anchorPane;
+        try {
+            anchorPane = (AnchorPane) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(app.getStage());
+            Scene scene = new Scene(anchorPane);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            ExceptionAlertView alertDialogView = new ExceptionAlertView(
+                    e.getMessage(),
+                    Arrays.toString(e.getStackTrace())
+            );
+            alertDialogView.showErrorMessage();
+        }
+    }
 }
