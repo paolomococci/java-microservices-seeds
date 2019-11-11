8560312e87ad34822a357c04965c433bcc28e82c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-11 18:17:21 +0100

    Update RootController.java

--- pm-jms-openjfx/src/main/java/local/example/seed/controller/RootController.java
+++ pm-jms-openjfx/src/main/java/local/example/seed/controller/RootController.java
@@ -16,224 +16,102 @@
  *
  */
 
-package local.example.seed;
+package local.example.seed.controller;
 
-import java.io.IOException;
-import java.util.Arrays;
+import javafx.fxml.FXML;
+import local.example.seed.App;
+import local.example.seed.view.alert.MenuItemAboutView;
 
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import local.example.seed.controller.RootController;
-import local.example.seed.controller.SeedEditorController;
-import local.example.seed.controller.SeedViewController;
-import local.example.seed.model.Seed;
-import local.example.seed.view.alert.ExceptionAlertView;
-import local.example.seed.view.init.InitView;
+public class RootController {
 
-/**
- * openjfx model-view-controller
- */
-public class App
-        extends Application {
+    private App app;
 
-    private static Scene scene;
-    private Stage stage;
-    private BorderPane borderPane;
-    private final ObservableList<Seed> seedValues = FXCollections.observableArrayList();
-
-    public App() {
-    }
-
-    @Override
-    public void start(Stage stage)
-            throws IOException {
-        this.stage = stage;
-        this.stage.setTitle("seed mvc application");
-        this.initRootView();
-        this.initSeedView();
-    }
-
-    public void initRootView() {
-        FXMLLoader rootLoader = new FXMLLoader();
-        rootLoader
-                .setLocation(App.class.getResource("view/root.fxml"));
-        try {
-            borderPane = (BorderPane) rootLoader.load();
-            scene = new Scene(borderPane, 640, 380);
-            stage.setScene(scene);
-            RootController rootController = rootLoader.getController();
-            rootController.setApp(this);
-            stage.show();
-        } catch (IOException e) {
-            ExceptionAlertView alertDialogView = new ExceptionAlertView(
-                    e.getMessage(),
-                    Arrays.toString(e.getStackTrace())
-            );
-            alertDialogView.showErrorMessage();
-        }
-    }
-
-    public void initSeedView() {
-        FXMLLoader seedViewLoader = new FXMLLoader();
-        seedViewLoader
-                .setLocation(App.class.getResource("view/seed-view.fxml"));
-        AnchorPane seedAnchorPane;
-        try {
-            seedAnchorPane = (AnchorPane) seedViewLoader.load();
-            borderPane.setCenter(seedAnchorPane);
-            SeedViewController seedViewController = seedViewLoader.getController();
-            seedViewController.setApp(this);
-        } catch (IOException e) {
-            ExceptionAlertView alertDialogView = new ExceptionAlertView(
-                    e.getMessage(),
-                    Arrays.toString(e.getStackTrace())
-            );
-            alertDialogView.showErrorMessage();
-        }
-    }
-
-    public boolean initSeedEditView(Seed seed) {
-        FXMLLoader seedEditorViewLoader = new FXMLLoader();
-        seedEditorViewLoader.setLocation(App.class.getResource("view/seed-editor.fxml"));
-        AnchorPane seedEditorAnchorPane;
-        try {
-            seedEditorAnchorPane = (AnchorPane) seedEditorViewLoader.load();
-            Stage seedEditorStage = new Stage();
-            seedEditorStage.setTitle("seed editor");
-            seedEditorStage.initModality(Modality.WINDOW_MODAL);
-            seedEditorStage.initOwner(this.stage);
-            Scene seedEditorScene = new Scene(seedEditorAnchorPane);
-            seedEditorStage.setScene(seedEditorScene);
-            SeedEditorController seedEditController = seedEditorViewLoader.getController();
-            seedEditController.setEditorStage(seedEditorStage);
-            seedEditController.setSeed(seed);
-            seedEditorStage.showAndWait();
-            return seedEditController.isOnClicked();
-        } catch (IOException e) {
-            ExceptionAlertView alertDialogView = new ExceptionAlertView(
-                    e.getMessage(),
-                    Arrays.toString(e.getStackTrace())
-            );
-            alertDialogView.showErrorMessage();
-            return false;
-        }
-    }
-
-    public void initPieChartView() {
-        InitView.init(
-                this, 
-                "view/pie-chart-view.fxml", 
-                "red wine blend pie chart");
-    }
-
-    public void initLineChartView() {
-        InitView.init(
-                this, 
-                "view/line-chart-view.fxml", 
-                "native vine line chart");
-    }
-
-    public void initAreaChartView() {
-        InitView.init(
-                this, 
-                "view/area-chart-view.fxml", 
-                "pseudo-random native vine area chart");
-    }
-
-    public void initBubbleChartView() {
-        InitView.init(
-                this, 
-                "view/bubble-chart-view.fxml", 
-                "pseudo-random native vine bubble chart");
-    }
-
-    public void initScatterChartView() {
-        InitView.init(
-                this, 
-                "view/scatter-chart-view.fxml", 
-                "pseudo-random native vine scatter chart");
-    }
-
-    public void initBarChartView() {
-        InitView.init(
-                this, 
-                "view/bar-chart-view.fxml", 
-                "pseudo-random native vine bar chart"
-        );
-    }
-
-    public void initStackedBarChartView() {
-        InitView.init(
-                this, 
-                "view/stacked-bar-chart-view.fxml", 
-                "pseudo-random native vine stacked bar chart");
-    }
-
-    public void initStackedAreaChartView() {
-        InitView.init(
-                this, 
-                "view/stacked-area-chart-view.fxml", 
-                "pseudo-random native vine stacked area chart");
-    }
-
-    public void initTemplate3DView() {
-        InitView.init(
-                this, 
-                "view/template-3d-view.fxml", 
-                "template3D");
-    }
-
-    public void initSampleOneView() {
-        InitView.init(
-                this, 
-                "view/sample-one-view.fxml", 
-                "sample one 3D");
-    }
-
-    public void initSampleTwoView() {
-        InitView.init(
-                this, 
-                "view/sample-two-view.fxml", 
-                "sample two 3D");
-    }
-
-    public void initSampleThreeView() {
-        InitView.init(
-                this, 
-                "view/sample-three-view.fxml", 
-                "sample three 3D");
+    public void setApp(App app) {
+        this.app = app;
     }
     
-    public void initSampleFourView() {
-        InitView.init(
-                this, 
-                "view/sample-four-view.fxml", 
-                "sample four 3D");
+    @FXML
+    private void menuItemQuit() {
+        System.exit(0);
     }
     
-    public void initSampleFiveView() {
-        InitView.init(
-                this, 
-                "view/sample-five-view.fxml", 
-                "sample five 3D");
+    @FXML
+    private void menuItemPieChart() {
+        app.initPieChartView();
     }
-
-    public Stage getStage() {
-        return stage;
+    
+    @FXML
+    private void menuItemBubbleChart() {
+        app.initBubbleChartView();
     }
-
-    public static void main(String[] args) {
-        launch(args);
+    
+    @FXML
+    private void menuItemScatterChart() {
+        app.initScatterChartView();
     }
-
-    public ObservableList<Seed> getSeedData() {
-        return seedValues;
+    
+    @FXML
+    private void menuItemBarChart() {
+        app.initBarChartView();
+    }
+    
+    @FXML
+    private void menuItemStackedBarChart() {
+        app.initStackedBarChartView();
+    }
+    
+    @FXML
+    private void menuItemLineChart() {
+        app.initLineChartView();
+    }
+    
+    @FXML
+    private void menuItemAreaChart() {
+        app.initAreaChartView();
+    }
+    
+    @FXML
+    private void menuItemStackedAreaChart() {
+        app.initStackedAreaChartView();
+    }
+    
+    @FXML
+    private void menuItemTemplate3D() {
+        app.initTemplate3DView();
+    }
+    
+    @FXML
+    private void menuItemSampleOne() {
+        app.initSampleOneView();
+    }
+    
+    @FXML
+    private void menuItemSampleTwo() {
+        app.initSampleTwoView();
+    }
+    
+    @FXML
+    private void menuItemSampleThree() {
+        app.initSampleThreeView();
+    }
+    
+    @FXML
+    private void menuItemSampleFour() {
+        app.initSampleFourView();
+    }
+    
+    @FXML
+    private void menuItemSampleFive() {
+        app.initSampleFiveView();
+    }
+    
+    @FXML
+    private void menuItemCanvasOne() {
+        app.initCanvasOneView();
+    }
+    
+    @FXML
+    private void menuItemAbout() {
+        MenuItemAboutView.showAlert();
     }
 }
