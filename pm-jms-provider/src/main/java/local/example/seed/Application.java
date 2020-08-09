e667e32db987169ff16716a0e71af28d503aa476
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-09 11:28:00 +0200

    Update Application.java

--- pm-jms-provider/src/main/java/local/example/seed/Application.java
+++ pm-jms-provider/src/main/java/local/example/seed/Application.java
@@ -20,8 +20,13 @@ package local.example.seed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		MongoAutoConfiguration.class,
+		MongoDataAutoConfiguration.class
+})
 public class Application {
 
 	public static void main(String[] args) {
