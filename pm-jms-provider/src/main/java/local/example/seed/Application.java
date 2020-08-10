7ba6f5c4ec6c08d923aa38ce1fd01d84483a1a61
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-10 17:02:43 +0200

    Update Application.java

--- pm-jms-provider/src/main/java/local/example/seed/Application.java
+++ pm-jms-provider/src/main/java/local/example/seed/Application.java
@@ -18,11 +18,15 @@
 
 package local.example.seed;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication(
@@ -37,9 +41,20 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 		basePackages = {"local.example.seed.repository"}
 		)
 @EntityScan("local.example.seed.model")
-public class Application {
+public class Application
+		extends AbstractReactiveMongoConfiguration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public MongoClient mongoClient() {
+		return MongoClients.create();
+	}
+
+	@Override
+	protected String getDatabaseName() {
+		return "yourdatabasename";
 	}
 }
