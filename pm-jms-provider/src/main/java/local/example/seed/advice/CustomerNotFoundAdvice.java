e4621f7808b22e6d1868b29b6d335e586e973cda
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-13 07:52:01 +0200

    Update CustomerNotFoundAdvice.java

--- pm-jms-provider/src/main/java/local/example/seed/advice/CustomerNotFoundAdvice.java
+++ pm-jms-provider/src/main/java/local/example/seed/advice/CustomerNotFoundAdvice.java
@@ -18,5 +18,20 @@
 
 package local.example.seed.advice;
 
+import local.example.seed.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class CustomerNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(CustomerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String CustomerNotFoundHandler(CustomerNotFoundException customerNotFoundException) {
+        return customerNotFoundException.getMessage();
+    }
 }
