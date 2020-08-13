cb7a2dd164735ed0ec62b14a792d4ca9e886be2b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-13 07:53:34 +0200

    Update ItemNotFoundAdvice.java

--- pm-jms-provider/src/main/java/local/example/seed/advice/ItemNotFoundAdvice.java
+++ pm-jms-provider/src/main/java/local/example/seed/advice/ItemNotFoundAdvice.java
@@ -18,5 +18,20 @@
 
 package local.example.seed.advice;
 
+import local.example.seed.exception.ItemNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ItemNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(ItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String ItemNotFoundHandler(ItemNotFoundException itemNotFoundException) {
+        return itemNotFoundException.getMessage();
+    }
 }
