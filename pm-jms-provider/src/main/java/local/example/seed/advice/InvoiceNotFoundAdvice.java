1dd5887c1db2f8be7a92943a20e3f74a4b54175a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-13 07:52:37 +0200

    Update InvoiceNotFoundAdvice.java

--- pm-jms-provider/src/main/java/local/example/seed/advice/InvoiceNotFoundAdvice.java
+++ pm-jms-provider/src/main/java/local/example/seed/advice/InvoiceNotFoundAdvice.java
@@ -18,5 +18,20 @@
 
 package local.example.seed.advice;
 
+import local.example.seed.exception.InvoiceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class InvoiceNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(InvoiceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String InvoiceNotFoundHandler(InvoiceNotFoundException invoiceNotFoundException) {
+        return invoiceNotFoundException.getMessage();
+    }
 }
