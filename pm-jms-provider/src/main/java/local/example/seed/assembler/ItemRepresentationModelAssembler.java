1355fe74b89d8af6cb774296e730969e797b2266
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-13 07:58:11 +0200

    Update ItemRepresentationModelAssembler.java

--- pm-jms-provider/src/main/java/local/example/seed/assembler/ItemRepresentationModelAssembler.java
+++ pm-jms-provider/src/main/java/local/example/seed/assembler/ItemRepresentationModelAssembler.java
@@ -18,5 +18,23 @@
 
 package local.example.seed.assembler;
 
-public class ItemRepresentationModelAssembler {
+import local.example.seed.model.Item;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemRepresentationModelAssembler
+        implements RepresentationModelAssembler<Item, EntityModel<Item>> {
+    @Override
+    public EntityModel<Item> toModel(Item item) {
+        return null;
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Item>>
+            toCollectionModel(Iterable<? extends Item> items) {
+        return null;
+    }
 }
