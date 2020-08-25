24867737f8cd0c7b081a1622cffa565c6cae5fa7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-25 08:56:29 +0200

    Create InvoiceReactiveCrudRestRepository.java

---
+++ pm-jms-reactive-provider/src/main/java/local/example/seed/repository/InvoiceReactiveCrudRestRepository.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.repository;
+
+import local.example.seed.document.Invoice;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import reactor.core.publisher.Mono;
+
+@RepositoryRestResource
+public interface InvoiceReactiveCrudRestRepository
+        extends ReactiveCrudRepository<Invoice, String> {
+
+    Mono<Invoice> findByCode(@Param("code") String code);
+}
