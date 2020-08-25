3647258a46c74bd3e76fe122aa73815a0a75f3d7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-25 07:34:42 +0200

    Update ItemView.java

--- pm-jms-consumer/src/main/java/local/example/seed/view/ItemView.java
+++ pm-jms-consumer/src/main/java/local/example/seed/view/ItemView.java
@@ -50,6 +50,7 @@ public class ItemView
         this.itemGrid.addColumn(item -> item.getCode()).setHeader("code").setSortable(true).setTextAlign(ColumnTextAlign.START);
         this.itemGrid.addColumn(item -> item.getName()).setHeader("name").setSortable(true);
         this.itemGrid.addColumn(item -> item.getDescription()).setHeader("description").setSortable(false);
+        this.itemGrid.addColumn(item -> item.getPrice()).setHeader("price").setSortable(true);
         this.retrieveButton = new Button(
                 "recovers all items",
                 VaadinIcon.ARROW_CIRCLE_DOWN_O.create(),
