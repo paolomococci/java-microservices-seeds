fd5b29184e387d245a2f6b0bd558325cc59ce315
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-27 06:55:25 +0200

    Update CustomerRestfulReactiveController.java

--- pm-jms-admin/src/main/java/local/example/seed/controller/reactive/CustomerRestfulReactiveController.java
+++ pm-jms-admin/src/main/java/local/example/seed/controller/reactive/CustomerRestfulReactiveController.java
@@ -18,11 +18,39 @@
 
 package local.example.seed.controller.reactive;
 
+import local.example.seed.model.Customer;
+import org.springframework.web.reactive.function.client.WebClient;
+
 public class CustomerRestfulReactiveController {
 
-    private static final String CUSTOMERS_URI = "http://127.0.0.1:8080/customers";
+    private static final String CUSTOMERS_URI = "http://127.0.0.1:8081/customers";
+    private final WebClient webClient;
 
     public CustomerRestfulReactiveController() {
+        this.webClient = WebClient.create();
+    }
 
+    public void create(Customer customer) {
+        // TODO
+    }
+
+    public void read(String id) {
+        // TODO
+    }
+
+    public void readAll() {
+        // TODO
+    }
+
+    public void update(Customer customer, String id) {
+        // TODO
+    }
+
+    public void partialUpdate(Customer customer, String id) {
+        // TODO
+    }
+
+    public void delete(String id) {
+        // TODO
     }
 }
