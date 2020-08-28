5bc47b9c081a47f02e5415dd00267f76acd9ee11
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-28 10:50:01 +0200

    Create ItemEditorView.java

---
+++ pm-jms-admin/src/main/java/local/example/seed/view/editor/ItemEditorView.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.view.editor;
+
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import local.example.seed.layout.MainLayout;
+
+@Route(value = "item-editor", layout = MainLayout.class)
+@PageTitle(value = "item editor")
+public class ItemEditorView {
+}
