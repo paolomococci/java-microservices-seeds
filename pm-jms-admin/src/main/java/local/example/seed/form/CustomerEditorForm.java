c8ed3ba978889e0b5e821bfa6d7e51a2e502b612
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-30 23:07:00 +0200

    Update CustomerEditorForm.java

--- pm-jms-admin/src/main/java/local/example/seed/form/CustomerEditorForm.java
+++ pm-jms-admin/src/main/java/local/example/seed/form/CustomerEditorForm.java
@@ -95,8 +95,8 @@ public class CustomerEditorForm
         // TODO
     }
 
-    public void setCustomerChangeHandler() {
-        // TODO
+    public void setCustomerChangeHandler(CustomerChangeHandler customerChangeHandler) {
+        this.customerChangeHandler = customerChangeHandler;
     }
 
     public final void editItem(Customer temp) {
