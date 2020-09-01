d1e77a2870b641e635e811e3e4a64455383b7f82
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-01 22:51:42 +0200

    Update CustomerEditorReactiveForm.java

--- pm-jms-admin/src/main/java/local/example/seed/form/reactive/CustomerEditorReactiveForm.java
+++ pm-jms-admin/src/main/java/local/example/seed/form/reactive/CustomerEditorReactiveForm.java
@@ -131,9 +131,12 @@ public class CustomerEditorReactiveForm
         if (alreadyExisting) {
             // TODO
         } else {
-            // TODO
+            this.customer = temp;
         }
-        // TODO
+        this.cancel.setVisible(alreadyExisting);
+        this.customerBinder.setBean(this.customer);
+        this.setVisible(true);
+        this.name.focus();
     }
 
     public void setCustomerChangeHandler(CustomerChangeHandler customerChangeHandler) {
