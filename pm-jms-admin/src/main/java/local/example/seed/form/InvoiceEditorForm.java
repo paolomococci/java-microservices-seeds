b1d642309e08d0df2c5e3abda8596cad3a92a43d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-01 22:54:57 +0200

    Update InvoiceEditorForm.java

--- pm-jms-admin/src/main/java/local/example/seed/form/InvoiceEditorForm.java
+++ pm-jms-admin/src/main/java/local/example/seed/form/InvoiceEditorForm.java
@@ -36,6 +36,8 @@ import local.example.seed.handler.InvoiceChangeHandler;
 import local.example.seed.model.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.net.URISyntaxException;
+
 @UIScope
 @SpringComponent
 public class InvoiceEditorForm
@@ -130,7 +132,11 @@ public class InvoiceEditorForm
         String id = temp.getId();
         final boolean alreadyExisting = temp.getId() != null;
         if (alreadyExisting) {
-            // TODO
+            try {
+                this.invoice = this.invoiceRestfulController.read(id);
+            } catch (URISyntaxException uriSyntaxException) {
+                uriSyntaxException.printStackTrace();
+            }
         } else {
             this.invoice = temp;
         }
