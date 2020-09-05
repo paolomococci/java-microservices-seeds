99b797c762d97e3663550eb65a3005822b77755e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-05 07:03:10 +0200

    Update InvoiceRestfulReactiveController.java

--- pm-jms-admin/src/main/java/local/example/seed/controller/reactive/InvoiceRestfulReactiveController.java
+++ pm-jms-admin/src/main/java/local/example/seed/controller/reactive/InvoiceRestfulReactiveController.java
@@ -21,6 +21,9 @@ package local.example.seed.controller.reactive;
 import local.example.seed.model.Invoice;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Collection;
+import java.util.stream.Stream;
+
 public class InvoiceRestfulReactiveController {
 
     private static final String INVOICE_REACTIVE_BASE_URI = "http://127.0.0.1:8080/api/restful/invoices";
@@ -41,6 +44,16 @@ public class InvoiceRestfulReactiveController {
 
     public void readAll() {
         // TODO
+    }
+
+    public Collection<Invoice> collectionOfAllInvoices() {
+        // TODO
+        return null;
+    }
+
+    public Stream<Invoice> streamOfAllInvoices() {
+        // TODO
+        return null;
     }
 
     public void update(Invoice invoice, String id) {
