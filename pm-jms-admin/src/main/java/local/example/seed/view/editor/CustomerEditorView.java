326d44bfc0b5a24881db01694b0111dd5fbb2900
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-06 07:23:56 +0200

    Update CustomerEditorView.java

--- pm-jms-admin/src/main/java/local/example/seed/view/editor/CustomerEditorView.java
+++ pm-jms-admin/src/main/java/local/example/seed/view/editor/CustomerEditorView.java
@@ -78,7 +78,7 @@ public class CustomerEditorView
         );
         this.filterEmailField.addValueChangeListener(
                 listener -> {
-                    // TODO
+                    this.showCustomerList(listener.getValue());
                 }
         );
 
@@ -101,10 +101,10 @@ public class CustomerEditorView
                 }
         );
 
-        this.showCustomerList();
+        this.showCustomerList("");
     }
 
-    private void showCustomerList() {
+    private void showCustomerList(String email) {
         this.customerGrid.setItems(
                 this.customerRestfulController.collectionOfAllCustomers()
         );
