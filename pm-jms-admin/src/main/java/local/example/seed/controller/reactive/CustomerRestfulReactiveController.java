c055459ebed8ebcb1d98e44abb9eb60d322930e0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-07 06:18:42 +0200

    Update CustomerRestfulReactiveController.java

--- pm-jms-admin/src/main/java/local/example/seed/controller/reactive/CustomerRestfulReactiveController.java
+++ pm-jms-admin/src/main/java/local/example/seed/controller/reactive/CustomerRestfulReactiveController.java
@@ -76,7 +76,7 @@ public class CustomerRestfulReactiveController {
                 .uri("/"+id)
                 .retrieve()
                 .onStatus(
-                        httpStatus -> HttpStatus.NOT_FOUND.equals(httpStatus),
+                        HttpStatus.NOT_FOUND::equals,
                         clientResponse -> Mono.empty()
                 )
                 .bodyToMono(Customer.class)
