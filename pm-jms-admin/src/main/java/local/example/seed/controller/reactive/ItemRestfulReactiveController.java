6b7047099158d1de37b9c645458529423b152295
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-07 06:17:59 +0200

    Update ItemRestfulReactiveController.java

--- pm-jms-admin/src/main/java/local/example/seed/controller/reactive/ItemRestfulReactiveController.java
+++ pm-jms-admin/src/main/java/local/example/seed/controller/reactive/ItemRestfulReactiveController.java
@@ -76,7 +76,7 @@ public class ItemRestfulReactiveController {
                 .uri("/"+id)
                 .retrieve()
                 .onStatus(
-                        httpStatus -> HttpStatus.NOT_FOUND.equals(httpStatus),
+                        HttpStatus.NOT_FOUND::equals,
                         clientResponse -> Mono.empty()
                 )
                 .bodyToMono(Item.class)
