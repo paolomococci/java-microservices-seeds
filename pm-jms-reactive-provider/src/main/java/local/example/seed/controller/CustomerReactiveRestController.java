177ec533ad0cb2ca2923ff202c2ef1df9a2c12ca
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-09 11:53:40 +0200

    Update CustomerReactiveRestController.java

--- pm-jms-reactive-provider/src/main/java/local/example/seed/controller/CustomerReactiveRestController.java
+++ pm-jms-reactive-provider/src/main/java/local/example/seed/controller/CustomerReactiveRestController.java
@@ -32,7 +32,6 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.Objects;
 
 @RepositoryRestController
