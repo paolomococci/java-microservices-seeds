a805072ee025e94475c988e84de82e79d1afd89a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-11 15:36:14 +0200

    Create CustomerRestfulRetrieverService.java

---
+++ pm-jms-reactive-admin/src/main/java/local/example/seed/service/CustomerRestfulRetrieverService.java
@@ -0,0 +1,46 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.service;
+
+import local.example.seed.model.Customer;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class CustomerRestfulRetrieverService {
+
+    public static List<Customer> getListOfCustomers(URI uri) {
+        Traverson traverson = new Traverson(uri, MediaTypes.HAL_JSON);
+        Traverson.TraversalBuilder traversalBuilder = traverson.follow("customers");
+        ParameterizedTypeReference<CollectionModel<Customer>> parameterizedTypeReference;
+        parameterizedTypeReference = new ParameterizedTypeReference<>() {};
+        CollectionModel<Customer> collectionModelOfCustomers;
+        collectionModelOfCustomers = traversalBuilder.toObject(parameterizedTypeReference);
+        Collection<Customer> collectionOfCustomers = collectionModelOfCustomers.getContent();
+        return new ArrayList<>(collectionOfCustomers);
+    }
+}
