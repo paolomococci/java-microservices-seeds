a75566d521b71be768c2cc03fe4f454ccec4258e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-13 00:58:19 +0200

    Update InvoiceReactiveRestController.java

--- pm-jms-reactive-provider/src/main/java/local/example/seed/controller/InvoiceReactiveRestController.java
+++ pm-jms-reactive-provider/src/main/java/local/example/seed/controller/InvoiceReactiveRestController.java
@@ -21,81 +21,44 @@ package local.example.seed.controller;
 import local.example.seed.assembler.InvoiceRepresentationModelAssembler;
 import local.example.seed.document.Invoice;
 import local.example.seed.repository.InvoiceReactiveCrudRestRepository;
-import local.example.seed.service.InvoiceReactiveMongoDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 @RepositoryRestController
 @RequestMapping(value = "/api/reactive/invoices", produces = "application/hal+json")
-public class InvoiceReactiveRestController { // TODO: totally to be reviewed
+public class InvoiceReactiveRestController {
 
     @Autowired
     InvoiceReactiveCrudRestRepository invoiceReactiveCrudRestRepository;
-
-    @Autowired
-    InvoiceReactiveMongoDBService invoiceReactiveMongoDBService;
 
     @Autowired
     InvoiceRepresentationModelAssembler invoiceRepresentationModelAssembler;
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody Invoice invoice) {
-        Mono<Invoice> result = this.invoiceReactiveCrudRestRepository.save(invoice);
-        if (result != null) {
-            EntityModel<Invoice> entityModelOfInvoice = this.invoiceRepresentationModelAssembler.toModel(
-                    Objects.requireNonNull(result.block())
-            );
-            return new ResponseEntity<>(entityModelOfInvoice, HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
-        }
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<?> read(@PathVariable String id) {
-        Mono<Invoice> result = this.invoiceReactiveCrudRestRepository.findById(id);
-        if (result != null) {
-            EntityModel<Invoice> entityModelOfInvoice = this.invoiceRepresentationModelAssembler.toModel(
-                    Objects.requireNonNull(result.block())
-            );
-            return new ResponseEntity<>(entityModelOfInvoice, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @GetMapping(path = "/code/{code}")
     public ResponseEntity<?> readByCode(@PathVariable String code) {
-        Mono<Invoice> result = this.invoiceReactiveCrudRestRepository.findByCode(code);
-        if (result != null) {
-            EntityModel<Invoice> entityModelOfInvoice = this.invoiceRepresentationModelAssembler.toModel(
-                    Objects.requireNonNull(result.block())
-            );
-            return new ResponseEntity<>(entityModelOfInvoice, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @GetMapping
     public ResponseEntity<?> readAll() {
-        Flux<Invoice> results = this.invoiceReactiveCrudRestRepository.findAll();
-        if (results != null) {
-            CollectionModel<EntityModel<Invoice>> collectionModelOfInvoices = this.invoiceRepresentationModelAssembler
-                    .toCollectionModel((Iterable<? extends Invoice>) results);
-            return new ResponseEntity<>(collectionModelOfInvoices, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PutMapping(path = "/{id}")
@@ -112,12 +75,7 @@ public class InvoiceReactiveRestController { // TODO: totally to be reviewed
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<?> delete(@PathVariable String id) {
-        Mono<Invoice> result = this.invoiceReactiveCrudRestRepository.findById(id);
-        if (result != null) {
-            this.invoiceReactiveCrudRestRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
