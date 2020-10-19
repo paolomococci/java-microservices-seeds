2693eafeb34d557d0c8b7a22a5fc429df99ecfb5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-19 20:52:24 +0200

    Update ItemRestfulRetrieverService.java

--- pm-jms-admin/src/main/java/local/example/seed/service/ItemRestfulRetrieverService.java
+++ pm-jms-admin/src/main/java/local/example/seed/service/ItemRestfulRetrieverService.java
@@ -26,6 +26,7 @@ import org.springframework.hateoas.client.Traverson;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -41,6 +42,9 @@ public class ItemRestfulRetrieverService {
         CollectionModel<Item> collectionModelOfItems;
         collectionModelOfItems = traversalBuilder.toObject(parameterizedTypeReference);
         Collection<Item> collectionOfItems = collectionModelOfItems.getContent();
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println(timestamp + " --- INFO --- number of customers: " + collectionOfItems.size());
+        collectionOfItems.forEach(System.out::println);
         return new ArrayList<>(collectionOfItems);
     }
 }
