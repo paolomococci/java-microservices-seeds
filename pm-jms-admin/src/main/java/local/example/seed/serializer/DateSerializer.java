4bb2d67c889d88b5f135950420c577e8b4b57fb8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-21 23:33:49 +0200

    Create DateSerializer.java

---
+++ pm-jms-admin/src/main/java/local/example/seed/serializer/DateSerializer.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.sql.Date;
+
+public class DateSerializer
+        extends StdSerializer<Date> {
+
+    protected DateSerializer(StdSerializer<?> src) {
+        super(src);
+    }
+
+    @Override
+    public void serialize(
+            Date date,
+            JsonGenerator jsonGenerator,
+            SerializerProvider serializerProvider
+    ) throws IOException {
+        // TODO
+    }
+}
