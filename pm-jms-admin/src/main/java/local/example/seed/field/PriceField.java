b3c92c7f459e8bf56d8c8ad8250eeb86882206b3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-22 18:14:08 +0200

    Update PriceField.java

--- pm-jms-admin/src/main/java/local/example/seed/field/PriceField.java
+++ pm-jms-admin/src/main/java/local/example/seed/field/PriceField.java
@@ -28,13 +28,18 @@ public class PriceField
     private final TextField doubleValueField = new TextField();
 
     public PriceField() {
-        this.setLabel("price");
-        this.doubleValueField.setPattern("[0-9]{0,3}\\.[0-9]{0,2}");
+        this.doubleValueField.setLabel("accepted format: nnn.nnn");
+        this.doubleValueField.setPattern("^([0-9]{0,3}([.][0-9]{0,2})?|[.][0-9]{0,2})$");
         this.doubleValueField.setPreventInvalidInput(true);
         this.doubleValueField.setMaxLength(6);
-        this.doubleValueField.setPlaceholder("000.00");
         HorizontalLayout horizontalLayout = new HorizontalLayout(this.doubleValueField);
+        horizontalLayout.setSpacing(false);
         this.add(horizontalLayout);
+    }
+
+    @Override
+    public void setValue(Double value) {
+        this.doubleValueField.setValue(value.toString());
     }
 
     @Override
@@ -44,8 +49,8 @@ public class PriceField
 
     @Override
     protected void setPresentationValue(Double newPresentationValue) {
-        if (newPresentationValue == null) {
-            this.doubleValueField.setValue("0.0");
+        if (newPresentationValue.isNaN()) {
+            this.doubleValueField.setValue("");
         }
     }
 }
