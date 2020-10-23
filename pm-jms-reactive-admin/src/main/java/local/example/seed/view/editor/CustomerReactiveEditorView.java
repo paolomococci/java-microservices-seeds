7ff19f2b38d02fa5f670262a6e9472e6ef7f54d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-23 17:52:41 +0200

    Update CustomerReactiveEditorView.java

--- pm-jms-reactive-admin/src/main/java/local/example/seed/view/editor/CustomerReactiveEditorView.java
+++ pm-jms-reactive-admin/src/main/java/local/example/seed/view/editor/CustomerReactiveEditorView.java
@@ -18,20 +18,42 @@
 
 package local.example.seed.view.editor;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import local.example.seed.controller.CustomerRestfulReactiveController;
 import local.example.seed.layout.MainLayout;
+import local.example.seed.model.Customer;
 
 @PageTitle(value = "customer reactive editor")
 @Route(value = "customer-reactive-editor", layout = MainLayout.class)
 public class CustomerReactiveEditorView
         extends Main {
 
+    private Grid<Customer> customerGrid;
+    private Binder<Customer> customerBinder;
+
+    private Customer customer;
+    private CustomerRestfulReactiveController customerRestfulReactiveController;
+
+    private TextField name;
+    private TextField surname;
+    private EmailField email;
+
+    private Button cancel;
+    private Button update;
+    private Button create;
+    private Button delete;
+
     public CustomerReactiveEditorView() {
     }
 
     private void showCustomerList(String email) {
-        
+        // TODO
     }
 }
