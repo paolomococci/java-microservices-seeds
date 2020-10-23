e8ecea0f12904c175c31650d9c83c51f2d26a639
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-23 10:11:58 +0200

    Create CustomerResponse.java

---
+++ pm-jms-reactive-admin/src/main/java/local/example/seed/response/CustomerResponse.java
@@ -0,0 +1,35 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.response;
+
+import local.example.seed.embedded.CustomerEmbedded;
+import local.example.seed.model.util.Link;
+import local.example.seed.model.util.Page;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerResponse {
+    CustomerEmbedded _embedded;
+    Link _links;
+    Page page;
+}
