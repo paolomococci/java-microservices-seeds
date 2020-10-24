fd9845449f12c18f9bb15ad24cc5e8b5fb1941d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-24 17:54:42 +0200

    Update ItemReactiveEditorView.java

--- pm-jms-reactive-admin/src/main/java/local/example/seed/view/editor/ItemReactiveEditorView.java
+++ pm-jms-reactive-admin/src/main/java/local/example/seed/view/editor/ItemReactiveEditorView.java
@@ -18,101 +18,43 @@
 
 package local.example.seed.view.editor;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.KeyModifier;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Main;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import local.example.seed.controller.ItemRestfulReactiveController;
-import local.example.seed.form.ItemEditorReactiveForm;
+import local.example.seed.field.PriceField;
 import local.example.seed.layout.MainLayout;
 import local.example.seed.model.Item;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @PageTitle(value = "item reactive editor")
 @Route(value = "item-reactive-editor", layout = MainLayout.class)
 public class ItemReactiveEditorView
         extends Main {
 
-    private final Grid<Item> itemGrid;
-    private final ItemEditorReactiveForm itemEditorReactiveForm;
-    private final ItemRestfulReactiveController itemRestfulReactiveController;
-    private final TextField filterCodeField;
-    private final Button addItem;
-    private final HorizontalLayout tools;
+    private Grid<Item> itemGrid;
+    private Binder<Item> itemBinder;
 
-    @Autowired
-    public ItemReactiveEditorView(
-            ItemEditorReactiveForm itemEditorReactiveForm,
-            ItemRestfulReactiveController itemRestfulReactiveController
-    ) {
-        super();
+    private Item item;
+    private ItemRestfulReactiveController itemRestfulReactiveController;
 
-        this.itemEditorReactiveForm = itemEditorReactiveForm;
-        this.itemRestfulReactiveController = itemRestfulReactiveController;
+    private TextField code;
+    private TextField name;
+    private TextField description;
+    private PriceField price;
 
-        this.itemGrid = new Grid<>();
-        this.itemGrid.addColumn(item -> item.getCode()).setHeader("code").setSortable(true).setTextAlign(ColumnTextAlign.START);
-        this.itemGrid.addColumn(item -> item.getName()).setHeader("name").setSortable(true);
-        this.itemGrid.addColumn(item -> item.getDescription()).setHeader("description").setSortable(false);
+    private Button cancel;
+    private Button update;
+    private Button create;
+    private Button delete;
 
-        this.itemGrid.asSingleSelect().addValueChangeListener(
-                listener -> {
-                    this.itemEditorReactiveForm.editItem(listener.getValue());
-                }
-        );
-
-        this.filterCodeField = new TextField();
-        this.filterCodeField.setPlaceholder("filter by code");
-        this.filterCodeField.setClearButtonVisible(true);
-        this.filterCodeField.setValueChangeMode(ValueChangeMode.LAZY);
-        this.filterCodeField.addFocusShortcut(
-                Key.KEY_F, KeyModifier.ALT
-        );
-        this.filterCodeField.addValueChangeListener(
-                listener -> {
-                    this.showItemList(listener.getValue());
-                }
-        );
-
-        this.addItem = new Button("add item", VaadinIcon.PLUS_CIRCLE_O.create());
-        this.addItem.addClickListener(
-                listener -> {
-                    this.itemGrid.asSingleSelect().clear();
-                    this.itemEditorReactiveForm.editItem(new Item());
-                }
-        );
-        this.addItem.addClickShortcut(Key.NUMPAD_ADD, KeyModifier.CONTROL);
-
-        this.tools = new HorizontalLayout(this.addItem);
-
-        this.add(this.itemGrid, this.tools, this.itemEditorReactiveForm);
-
-        this.itemEditorReactiveForm.setItemChangeHandler(
-                () -> {
-                    this.itemEditorReactiveForm.setVisible(false);
-                }
-        );
-
-        this.showItemList("");
+    public ItemReactiveEditorView() {
     }
 
     private void showItemList(String code) {
-        if (code.isEmpty() || code.isBlank()) {
-            this.itemGrid.setItems(
-                    this.itemRestfulReactiveController.collectionOfAllItems()
-            );
-        } else {
-            this.itemGrid.setItems(
-                    this.itemRestfulReactiveController.findByCode(code)
-            );
-        }
+        // TODO
     }
 }
