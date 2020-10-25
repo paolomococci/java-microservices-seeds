4f1ed7616caca8bbc17ecf082eff6ece2794e0c8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-25 14:11:47 +0100

    Update ItemResponse.java

--- pm-jms-reactive-admin/src/main/java/local/example/seed/response/ItemResponse.java
+++ pm-jms-reactive-admin/src/main/java/local/example/seed/response/ItemResponse.java
@@ -20,7 +20,6 @@ package local.example.seed.response;
 
 import local.example.seed.embedded.ItemEmbedded;
 import local.example.seed.model.util.Link;
-import local.example.seed.model.util.Page;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,6 +29,4 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ItemResponse {
     ItemEmbedded _embedded;
-    Link _links;
-    Page page;
 }
