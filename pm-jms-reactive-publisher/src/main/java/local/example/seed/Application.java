b28262193da48059402bdf9a4b887a5b3fe13206
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-26 11:43:44 +0100

    Create Application.java

---
+++ pm-jms-reactive-publisher/src/main/java/local/example/seed/Application.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@SpringBootApplication
+@EnableReactiveMongoRepositories(
+		basePackages = {
+				"local.example.seed.repository"
+		})
+@EntityScan("local.example.seed.document")
+public class Application {
+
+	private final String DATABASE_NAME = "sampledb";
+
+	@Autowired
+	MongoClient reactiveStreamsMongoClient;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ReactiveMongoTemplate reactiveMongoTemplate() {
+		return new ReactiveMongoTemplate(
+				reactiveStreamsMongoClient,
+				DATABASE_NAME
+		);
+	}
+}
+
