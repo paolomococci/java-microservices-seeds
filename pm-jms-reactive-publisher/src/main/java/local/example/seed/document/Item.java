d29b0d97e2e91ea0d36851d0441941ce261000a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-26 17:22:16 +0100

    Update Item.java

--- pm-jms-reactive-publisher/src/main/java/local/example/seed/document/Item.java
+++ pm-jms-reactive-publisher/src/main/java/local/example/seed/document/Item.java
@@ -18,16 +18,25 @@
 
 package local.example.seed.document;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Document(value = "item")
 public class Item {
 
     @Id
     private String id;
+
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING)
+    private String code;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
 }
