15e80b8cc79709905c4bed98885cc992771c52df
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-26 17:21:43 +0100

    Update Invoice.java

--- pm-jms-reactive-publisher/src/main/java/local/example/seed/document/Invoice.java
+++ pm-jms-reactive-publisher/src/main/java/local/example/seed/document/Invoice.java
@@ -18,16 +18,32 @@
 
 package local.example.seed.document;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Document(value = "invoice")
 public class Invoice {
 
     @Id
     private String id;
+
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING)
+    private String code;
+
+    private String date;
+
+    private Double total;
+
+    @DBRef
+    private Customer customer;
+
+    @DBRef
+    private List<Item> items;
 }
