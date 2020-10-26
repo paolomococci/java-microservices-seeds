7a7ce526e049ee8f2e24f67920e80c8b155bbca8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-26 17:24:15 +0100

    Update ItemReactiveMongoRestfulRepository.java

--- pm-jms-reactive-publisher/src/main/java/local/example/seed/repository/ItemReactiveMongoRestfulRepository.java
+++ pm-jms-reactive-publisher/src/main/java/local/example/seed/repository/ItemReactiveMongoRestfulRepository.java
@@ -20,9 +20,15 @@ package local.example.seed.repository;
 
 import local.example.seed.document.Item;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RepositoryRestResource
 public interface ItemReactiveMongoRestfulRepository
         extends ReactiveMongoRepository<Item, String> {
+
+    Mono<Item> findByCode(@Param("code") String code);
+    Flux<Item> findAllByName(@Param("name") String name);
 }
