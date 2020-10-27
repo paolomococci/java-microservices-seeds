50784ca6320554e6994f1437d897fa9687077a43
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-27 19:20:16 +0100

    Create ItemHttpVerbsRouterConfiguration.java

---
+++ pm-jms-reactive-publisher/src/main/java/local/example/seed/configuration/ItemHttpVerbsRouterConfiguration.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.seed.configuration;
+
+import local.example.seed.handler.ItemHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class ItemHttpVerbsRouterConfiguration {
+
+    private final MediaType mediaType = MediaTypes.HAL_JSON;
+
+    @Bean
+    public RouterFunction<ServerResponse> serverResponseRouterFunction(ItemHandler itemHandler) {
+        // TODO
+        return null;
+    }
+}
