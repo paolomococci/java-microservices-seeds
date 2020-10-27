ab44d6453b78f42ad2d7ffe748bebe6645b17860
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-27 19:01:53 +0100

    Update InvoiceReactiveMongoRestfulRepository.java

--- pm-jms-reactive-publisher/src/main/java/local/example/seed/repository/InvoiceReactiveMongoRestfulRepository.java
+++ pm-jms-reactive-publisher/src/main/java/local/example/seed/repository/InvoiceReactiveMongoRestfulRepository.java
@@ -25,7 +25,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "invoices", path = "invoices")
 public interface InvoiceReactiveMongoRestfulRepository
         extends ReactiveMongoRepository<Invoice, String> {
 
